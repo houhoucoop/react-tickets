e4992a52f77a7ec239ac38ccd0415d80ec853ad4
yiya <qq7886@hotmail.com>
2018-07-13 10:56:27 +0800

    express setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import MainSection from '../containers/MainSection';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <MainSection />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      testRes: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          testRes: data.express,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">{this.state.testRes}</p>
+        <Header />
+        <MainSection />
+      </div>
+    );
+  }
+}
 
 export default App;
